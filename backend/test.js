a9400a0e61e20e53d89ae975e243de797729f8a3
nithiyanantham <nithiyanantham02@gmail.com>
2026-03-17 11:11:45 +0000

    chore: sync backend → backend from repo1 [skip ci]

--- backend/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-console.log('Express loaded');
-try {
-    const axios = require('axios');
-    console.log('Axios loaded');
-} catch (e) {
-    console.error('Axios failed');
-}
